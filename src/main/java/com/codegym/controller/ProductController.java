fa2d9feec5178b3de9d8bd2b6e22e9524ba250d4
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-06-11 11:33:02 +0700

    init

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -4,13 +4,11 @@ import com.codegym.model.Product;
 import com.codegym.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes
 public class ProductController {
 
     @Autowired
--- src/main/java/com/codegym/service/impl/CartServiceImpl.java
+++ src/main/java/com/codegym/service/impl/CartServiceImpl.java
@@ -1,7 +1,28 @@
 package com.codegym.service.impl;
 
+import com.codegym.model.Product;
 import com.codegym.service.CartService;
 
-public class CartServiceImpl implements CartService {
+import java.util.List;
 
+public class CartServiceImpl implements CartService {
+    @Override
+    public List<Product> showAllCart() {
+        return null;
+    }
+
+    @Override
+    public void addQuantity(Long id) {
+
+    }
+
+    @Override
+    public void subtractQuantity(Long id) {
+
+    }
+
+    @Override
+    public void subtractAllQuantity(Long id) {
+
+    }
 }
